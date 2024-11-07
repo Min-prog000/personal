812ca2cd34a2b65637a2efb396d295969e804350
MinPlu <minprog-000@gmail.com>
2024-11-07 16:41:14 +0900

    文字列操作
    str_lower.py : 大文字小文字変換
    str_slice.py   : スライス構文

---
+++ Python/Dokusyu_Python/chap05/str_lower.py
@@ -0,0 +1,27 @@
+# 文字列の操作 - 大文字・小文字変換
+
+
+data1 = 'Wings Project'
+data2 = 'self learn python'
+data3 = 'Fußball'
+
+# lower : 小文字変換
+print(data1.lower())
+
+# upper : 大文字変換
+print(data1.upper())
+
+# swapcase : 大文字と小文字を反転
+print(data1.swapcase())
+
+# capitalize : 先頭文字を大文字に、残りを小文字に変換
+print(data2.capitalize())
+
+# title : 単語の先頭文字を大文字に、残りを小文字に変換
+#         空白区切りで単語とみなす
+print(data2.title())
+
+# casefold : 大文字小文字の区別をなくす
+#            英語以外の大文字も小文字にする(lowerでは小文字のまま)
+print(data3.lower())     # ß 変化なし
+print(data3.casefold())  # ß -> ss
